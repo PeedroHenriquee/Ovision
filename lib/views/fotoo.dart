e7d0049f69743c6bb200b4b4f667fb693d86c212
Lucas Gomes <lg.projectdev@gmail.com>
2022-10-07 20:41:47 -0300

    deu certo

--- lib/views/fotoo.dart
+++ lib/views/fotoo.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class Fotos extends StatefulWidget {
   @override
@@ -41,6 +43,48 @@ class _Fotostate extends State<Fotos> {
     });
   }
 
+  uploadImage() {
+    setState(() async {
+      // Create a storage reference from our app
+      ;
+      
+      
+
+// Create the file metadata
+      final metadata = SettableMetadata(contentType: "imageee/jpeg");
+
+// Create a reference to the Firebase Storage bucket
+      final storageRef = FirebaseStorage.instance.ref();
+
+// Upload file and metadata to the path 'images/mountains.jpg'
+      final uploadTask = storageRef.child("imagens").putFile(imageSelect);
+
+// Listen for state changes, errors, and completion of the upload.
+      uploadTask.snapshotEvents.listen((TaskSnapshot taskSnapshot) {
+        switch (taskSnapshot.state) {
+          case TaskState.running:
+            final progress = 100.0 *
+                (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes);
+            print("Upload is $progress% complete.");
+            break;
+          case TaskState.paused:
+            print("Upload is paused.");
+            break;
+          case TaskState.canceled:
+            print("Upload was canceled");
+            break;
+          case TaskState.error:
+            // Handle unsuccessful uploads
+            break;
+          case TaskState.success:
+            // Handle successful uploads on complete
+            // ...
+            break;
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,11 +94,11 @@ class _Fotostate extends State<Fotos> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             AspectRatio(
-              aspectRatio: 4/5,
-              child: imageSelect == null?
-              Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(32.0),
+              aspectRatio: 4 / 5,
+              child: imageSelect == null
+                  ? Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(32.0),
                       color: Colors.grey,
                       width: double.maxFinite,
                       height: 500,
@@ -75,28 +119,24 @@ class _Fotostate extends State<Fotos> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-               ElevatedButton(
+                ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 8),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15))
-                    ),
-                      
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 8),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15))),
                   onPressed: pickImageCamera,
                   child: const Icon(
                     Icons.camera_alt,
                     color: Color.fromARGB(255, 255, 255, 255),
                   ),
                 ),
-
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 8),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15))
-                    ),
-
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 8),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15))),
                   onPressed: pickImageGaleria,
                   child: const Icon(
                     Icons.photo_library,
@@ -105,6 +145,11 @@ class _Fotostate extends State<Fotos> {
                 ),
               ],
             ),
+            if (imageSelect != null)
+              ElevatedButton(
+                onPressed: uploadImage,
+                child: const Text("Salvar"),
+              ),
             if (imageSelect != null)
               ElevatedButton(
                 onPressed: clearImage,
